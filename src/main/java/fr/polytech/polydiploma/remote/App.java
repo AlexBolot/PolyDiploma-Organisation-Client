e8418d471af09b2a9be6bd2f9b398d95739eb1a5
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2019-05-02 08:54:16 +0200

    minor changes : App's port + updating command names in test.poly

--- src/main/java/fr/polytech/polydiploma/remote/App.java
+++ src/main/java/fr/polytech/polydiploma/remote/App.java
@@ -31,7 +31,7 @@ public class App extends Shell<PolydiplomaOrganisationPublicAPI> {
 
     public static void main(String[] args) {
         String host = ( args.length == 0 ? "localhost" : args[0] );
-        String port = ( args.length < 2  ? "8080"      : args[1] );
+        String port = ( args.length < 2  ? "8082"      : args[1] );
         System.out.println("\n\nLancement du client pour l'organisation de Polydiploma");
         System.out.println("  - Serveur distant: " + host);
         System.out.println("  - Numéro de port:  " + port);
